4ce522928bf585086ea3217819e5d6694177046a
Abdullah <sheheween1518@gmail.com>
2026-04-13 12:17:51 +0300

    * TODO-2 Define the schema of the DB

--- 7-1-mongodb/server.js
+++ 7-1-mongodb/server.js
@@ -196,7 +196,12 @@ mongoose.connect(uri)
 
 
 // define schema
-
+const studentSchema = new mongoose.Schema({
+   name: String,
+   age: Number,
+   major: String
+});
+const Student = mongoose.model("Student", studentSchema);
 
 // create document
 
